032efcd34e6c7888b5c406e13c983630a62145ab
Prabhjot-MH <jotiqa19@gmail.com>
2020-05-20 22:40:45 -0700

    My second attempt

--- src/Github/Gitprogram.java
+++ src/Github/Gitprogram.java
@@ -3,8 +3,8 @@ package Github;
 public class Gitprogram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("My name is Prabhjot");
 	}
 
 }
